1a47d239fbdf9be71bb3f2acf02bad35c215702f
Liz Berberena <contact@lizberberena.com>
2017-11-21 02:34:35 -0500

    Added some initial code to PlayGame class and Player class
    Committing this again because Player class didn't have the array for
    inventory when I committed these changes!

--- FinalProject/src/application/Player.java
+++ FinalProject/src/application/Player.java
@@ -13,4 +13,32 @@ package application;
 
 public class Player {
 
+/*
+A Player class which will
+i. Have all the relevant information you collect from the player such
+as location, name, etc.*/
+
+//instance variables
+private String name;
+
+public Player() {
+	this.name = "";
+	
+}
+
+public Player(String name) {
+	this.name = name;
+}
+
+public void setName(String name) {
+	this.name = name;
+	
+}
+
+public String getName() {
+	return this.name;
+	
+}
+
+	
 }//end of Player class

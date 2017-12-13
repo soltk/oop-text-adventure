85476bdd792cc6fe124c77ee907eea660ebd39e1
Huda Yousef <yousefhuda@gmail.com>
2017-12-13 00:25:15 -0500

    Updated Enemy class

--- FinalProject/src/application/Enemy.java
+++ FinalProject/src/application/Enemy.java
@@ -1,23 +1,17 @@
-/*
- * Written by:
- * 
- * Liz Berberena
- * Jack Davitt
- * Shazia Mansuri
- * Simran Nanwani
- * Huda Yousef
- * 
- */
-
-package application;
-
 import java.util.Random;
 
-public class Enemy extends StartAdventure{
+public class Enemy {
 
+	private int lives; // property of Enemy
+	
 	public Enemy(){
+		this.lives=0; //default lives attribute
 		
-	} // end of Enemy()
+	}// end of default constructor Enemy()
+	
+	public Enemy(int lives){
+		this.lives=lives;
+	} 
 	
 	public int attack(){
 		Random rand = new Random();
@@ -25,4 +19,28 @@ public class Enemy extends StartAdventure{
 		return damage;
 	}
 	
+	public boolean damage(int damageValue){ //method for damage
+		boolean enemyDead=false;
+		this.lives=this.lives-damageValue;
+		
+		if(this.lives<=0){
+			enemyDead=true;
+		}
+		return enemyDead;
+	}//end of damage method
+	
+	public void setLives(int lives) {
+		this.lives = lives;
+	}
+
+	public int getLives() {
+		return this.lives;
+	}
+	
+	public String toString(){
+		String displayLives = "The enemy's current lives are: " + this.lives;
+		return displayLives;
+	}
+
+
 }
